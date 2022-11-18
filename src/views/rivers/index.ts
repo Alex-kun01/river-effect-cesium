3b1bc9d63fabdaf84e8294091c64e25d3c4457e6
西南开发二组蒋治坤 <jiangzhikun@uino.com>
2022-11-18 09:20:55 +0800

    feat: poinst2

--- src/views/rivers/index.ts
+++ src/views/rivers/index.ts
@@ -2,7 +2,7 @@
  * @Author: 西南开发二组蒋治坤 jiangzhikun@uino.com
  * @Date: 2022-11-11 13:56:05
  * @LastEditors: 西南开发二组蒋治坤 jiangzhikun@uino.com
- * @LastEditTime: 2022-11-17 18:35:03
+ * @LastEditTime: 2022-11-18 09:20:10
  * @FilePath: \river-effect-cesium\src\views\rivers\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,12 +16,22 @@ const testPoints = [
     [116.03496881901276, 40.31773322241149]
 ];
 
+const test2Points = [
+    [116.28449899269205, 39.9657405601117],
+    [116.3409035972554, 40.13802096673991],
+    [116.46760954487726, 40.22247805983962],
+    [116.49354007960139, 40.12827628951574],
+    [116.51646965037305, 40.00816448146126],
+    [116.41442417273367, 39.83705408572517],
+    [116.29589100121882, 39.875180678513516]
+];
+
 
 export default function initx() {
     window.uino.app.create({
         type: 'GeoWater',
         name: 'save_type_name',
-        coordinates: testPoints,
+        coordinates: test2Points,
         renderer: {
             reflectionNormal: '/static/image/reavce.png', // 反射法线贴图
             refractionNormal: '/static/image/reavce.png', // 折射法线贴图
